e55335bfef6aae3b85d2102b7451422c6860aa5e
Saurabh Joshi <sauj123@hotmail.com>
2016-12-03 15:15:56 +1100

    Converted Header component to use semantic-ui-react elements

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,47 +1,51 @@
 import React, { Component } from 'react'
+import {Search, Grid, Container, Icon, Menu, Label, Dropdown, Popup} from "semantic-ui-react"
 import UnitSearch from './UnitSearch.jsx'
 
 class Header extends Component {
-    render () {
+    toggleToS() {
+
+    }
+    togglePrivacy() {
+
+    }
+    render() {
         return (
-            <div className="ui top attached inverted stackable menu">
-        <a className="header item">
-            <img className="logo" src="../resources/img/logo.png" alt="logo" />
-            monPlan Alpha
-        </a>
-
-        <div id="unitSearch" className="item">
-            <UnitSearch />
-        </div>
-
-        <div id="creditCounter" className="item">
-            <div className="ui green large label">
-                Total Credits Earnt
-                <div id="credits" className="detail">0</div>
-            </div>
-        </div>
-        <div className="right menu">
-            <div id="displayMessage" className="ui item pop" data-title="Everything looks good" data-content="As you add units, we will inform you of any
-                conflicts, such as missing prerequisites.">
-                Status:  <span id="statusTag">OK</span> <i id="statusIcon" className="icon green checkmark"></i>
-            </div>
-            <div className="ui normal dropdown icon item">
-                <i className="info icon"></i>
-                <div className="menu">
-                    <h4 className="ui inverted header">Quick Links (for Devs)</h4>
-                    <a href="https://github.com/MonashUnitPlanner" target="_blank" className="item"><i className="github icon"></i>GitHub Project</a>
-                    <a href="https://monplan.slack.com" className="item"><i className="slack icon"></i>Slack (for Devs)</a>
-                    <h4 className="ui inverted header">About</h4>
-                    <a href="https://gitreports.com/issue/MonashUnitPlanner/monPlan" target="_blank"  className="item"><i className="bug icon"></i> Submit an Issue</a>
-                    <a href="https://monashunitplanner.github.io" target="_blank"  className="item"><i className="info icon"></i>The Project</a>
-                    <a href="https://goo.gl/TO6Z3M" target="_blank" className="item"><i className="users icon"></i> Join the Team</a>
-                    <h4 className="ui inverted header">Our Policies</h4>
-                    <a className="item" onClick="toggleToS()">Terms of Use</a>
-                    <a className="item" onClick="togglePrivacy()">Privacy Policy</a><br />
-                </div>
-            </div>
-        </div>
-    </div>
+            <Menu attached="top" inverted stackable>
+                <Menu.Header className="item">
+                    <img className="logo" src="./img/logo.png" alt="logo" />
+                    monPlan Alpha
+                </Menu.Header>
+                <UnitSearch className="item" />
+                <Menu.Item id="creditCounter">
+                    <Label color="green" size="large">
+                        Total Credits Earnt
+                        <Label.Detail id="credits">0</Label.Detail>
+                    </Label>
+                </Menu.Item>
+                <Menu.Menu position="right">
+                    <Popup
+                        id="displayMessage"
+                        trigger={<Menu.Item>Status: <span id="statusTag">OK</span> <Icon name="checkmark" color="green" id="statusIcon" /></Menu.Item>}
+                        header="Everything looks good"
+                        content="As you add units, we will inform you of any conflicts, such as missing prerequisites."
+                        />
+                    <Dropdown icon="info">
+                        <Dropdown.Menu>
+                            <Dropdown.Header>Quick Links (for Devs)</Dropdown.Header>
+                            <Dropdown.Item as="a" href="https://github.com/MonashUnitPlanner" target="_blank"><Icon name="github" />GitHub Project</Dropdown.Item>
+                            <Dropdown.Item as="a" href="https://monplan.slack.com" target="_blank"><Icon name="slack" />Slack (for Devs)</Dropdown.Item>
+                            <Dropdown.Header>About</Dropdown.Header>
+                            <Dropdown.Item as="a" href="https://gitreports.com/issue/MonashUnitPlanner/monPlan" target="_blank"><i className="bug icon"></i> Submit an Issue</Dropdown.Item>
+                            <Dropdown.Item as="a" href="https://monashunitplanner.github.io" target="_blank"  className="item"><i className="info icon"></i>The Project</Dropdown.Item>
+                            <Dropdown.Item as="a" href="https://goo.gl/TO6Z3M" target="_blank" className="item"><i className="users icon"></i> Join the Team</Dropdown.Item>
+                            <Dropdown.Header as="a" className="ui inverted header">Our Policies</Dropdown.Header>
+                            <Dropdown.Item as="a" onClick={this.toggleToS.bind(this)}>Terms of Use</Dropdown.Item>
+                            <Dropdown.Item as="a" onClick={this.togglePrivacy.bind(this)}>Privacy Policy</Dropdown.Item><br />
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Menu.Menu>
+            </Menu>
         )
     }
 }
