2ea9afb793fafa765e520261bd4b783bcaf2008b
Joshua-Xavier <j.nelssonsmith@gmail.com>
2016-12-08 14:12:52 +1100

    hopefuly fixes merge issues

--- app/config/routes.jsx
+++ app/config/routes.jsx
@@ -7,12 +7,11 @@ import Home from "../components/Home.jsx";
 import Plan from "../components/Plan.jsx";
 
 /* Containers import here*/
-import UnitInfoContainer from "../containers/UnitInfoContainer.jsx";
+
 
 module.exports = (
   <Route path="/" component={Main}>
       <IndexRoute component={Home} />
       <Route path="plan" component={Plan} />
-      <Route path="unit" component={UnitInfoContainer} />
   </Route>
 );
