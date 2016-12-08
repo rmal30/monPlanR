5a4c76bc08d77eaa6118eb613ff73c42a2d1de83
lorderikir <lorderikir@github.com>
2016-12-08 13:48:43 +1100

    added license file, updated README.md and privacy.jsx

--- app/components/modals/privacy.jsx
+++ app/components/modals/privacy.jsx
@@ -29,7 +29,7 @@ class PrivacyModal extends Component {
                 <Modal.Content>
                     <Modal.Description>
                       We do not record any data across our services.  Your data will be recorded and saved into your browser's cache for future performance, to improve user experience with our site.
-                      As our site is currently hosted by GitHub Pages, we (being monPlan) do not track your activity, but the organisation at times, may track your activities.
+                      As our site is currently hosted by GitHub Pages, we (being monPlan) and eSolutions do not track your activity, but the organisation at times, may track your activities.
                       Within our site to display data, we also honor Do Not Track (DNT) Plugins, no features will be disabled if your browser uses a DNT plugin.
                       In future versions, if you are logged into our site, we will load data about your course via an API supplied by the University to help you plan out your degree and make sure you get the most out of your degree.
                     </Modal.Description>
