e487c2cccaa177190405dd7e002aedd0b9ed8611
Joshua-Xavier <j.nelssonsmith@gmail.com>
2016-12-12 09:16:49 +1100

    finished unitinfo documentation

--- app/components/Unit/UnitInfo.jsx
+++ app/components/Unit/UnitInfo.jsx
@@ -5,18 +5,18 @@ import UnitInfoPlaceholder from "./UnitInfoPlaceholder.jsx";
 import CollapseButton from "../CollapseButton.jsx";
 
 /**     
- * Description
+ * This component displays the unit info for a selected unit in the form of a collapsable tray.
  * @author JXNS
  * 
- * @param {boolean} collapse - 
- * @param {string} Faculty - 
- * @param {number} likeScore - 
- * @param {boolean} isDisabled -
- * @param {function} onCollapseClick - 
- * @param {string} Synopsis - 
- * @param {string} UnitCode - 
- * @param {string} UnitName - 
- * @param {number} usefulnessScore - 
+ * @param {boolean} collapse - A true/false value representing whether the component is in collpased state or not.
+ * @param {string} Faculty - The faculty a unit belongs to.
+ * @param {number} likeScore - A score between 0 and 5 representing how much the students enjoyed a unit.
+ * @param {boolean} isDisabled - A value representing whether the collapse button should be disabled, only necessary for before first search.
+ * @param {function} onCollapseClick - A function called when the prop collapses (in this case used to control the data and state of container parent).
+ * @param {string} Synopsis - A short description of the unit.
+ * @param {string} UnitCode - The unit's associated unit code.
+ * @param {string} UnitName - The unit's associated unit name.
+ * @param {number} usefulnessScore - A score between 0 and 5 representing how much students felt they learnt in the unit. 
  */
 function UnitInfo(props) {
 
