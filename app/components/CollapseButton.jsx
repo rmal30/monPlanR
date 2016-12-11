e928e18f8de223b2679ed67279f526b61ffef285
Joshua-Xavier <j.nelssonsmith@gmail.com>
2016-12-11 14:30:29 +1100

    Changed unit detail to not float anymore so it stacks correctly

--- app/components/CollapseButton.jsx
+++ app/components/CollapseButton.jsx
@@ -4,13 +4,13 @@ import { Grid, Icon, Button } from "semantic-ui-react";
 function CollapseButton(props){
     if(props.collapse){
         return(
-            <Button floated="right" onClick={props.onCollapseClick}>
+            <Button compact={true} onClick={props.onCollapseClick}>
                     Show unit details <Icon name="chevron down" />
             </Button>
         );
     } else {
         return (
-            <Button floated="right" onClick={props.onCollapseClick}>
+            <Button compact={true} onClick={props.onCollapseClick}>
                     Hide unit details <Icon name="chevron up" />
             </Button>
         );
