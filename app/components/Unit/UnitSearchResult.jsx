b1e42f358b50fedfb3fd41bd72c25be59ea1835b
Joshua-Xavier <j.nelssonsmith@gmail.com>
2016-12-12 10:00:50 +1100

    added unit search result docs

--- app/components/Unit/UnitSearchResult.jsx
+++ app/components/Unit/UnitSearchResult.jsx
@@ -1,18 +1,26 @@
 import React, { PropTypes } from "react";
 import { Rating, Icon } from "semantic-ui-react"
 
+/**
+ * Returns the way in which we want a search result to be rendered
+ * @author JXNS
+ * 
+ * @param {string} UnitCode - The unit code associated with a result.
+ * @param {string} UnitName - The unit name associated with a result.
+ */
 function UnitSearchResult(props) {
+
+    UnitSearchResult.propTypes = {
+        UnitCode: PropTypes.string.isRequired,
+        UnitName: PropTypes.string.isRequired,
+    }
+
     return (
         <div className="content">
             <div className="title">{props.UnitCode}</div>
             <div className="description">{props.UnitName}</div>
         </div>
     );
-}
-
-UnitSearchResult.propTypes = {
-    UnitCode: PropTypes.string.isRequired,
-    UnitName: PropTypes.string.isRequired,
 }
 
 export default UnitSearchResult;
