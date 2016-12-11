8fab5833b4530f7c9d5d41023775aef30fdece77
Joshua-Xavier <j.nelssonsmith@gmail.com>
2016-12-12 10:09:46 +1100

    trying to resolve conflicts

--- app/components/Unit/SetuRating.jsx
+++ app/components/Unit/SetuRating.jsx
@@ -1,21 +1,7 @@
 import React, { PropTypes } from "react";
 import { Rating, Icon } from "semantic-ui-react"
 
-/**
- * This is the setuRating component, it returns 2 ratings, one representing how much the student's felt they learnt from a unit, and one representing
- * how much they enjoyed a unit.
- * @author JXNS
- * 
- * @param {number} starRating - the level of student learning (0 to 5)
- * @param {number} heartRating - the level of student enjoyment (0 to 5)
- */
 function SetuRating(props) {
-    
-    SetuRating.propTypes = {
-        starRating: PropTypes.number.isRequired,
-        heartRating: PropTypes.number.isRequired
-    }
-
     return (
         <div id="setu-rating">
             <p>I learnt a lot</p>
@@ -26,6 +12,11 @@ function SetuRating(props) {
         </div>
         
     );
+}
+
+SetuRating.propTypes = {
+    starRating: PropTypes.number.isRequired,
+    heartRating: PropTypes.number.isRequired
 }
 
 export default SetuRating;
