7a0495b5d5ec2f0f79e46b49e70f3dd8090372c0
Joshua-Xavier <j.nelssonsmith@gmail.com>
2016-12-19 13:14:03 +1100

    started adding descriptiong container

---
+++ app/containers/UnitDescriptionContainer.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class UnitDescriptionContainer extends Component {
+
+    constructor(props) {
+        super(props);
+
+        
+
+
+        this.state = {
+            fullText: this.props.fullText,
+            shortText: text,
+            hide: this.props.hide
+        };
+
+
+    }
+
+    render() {
+        return (
+
+        );
+    }
+}
