3288bd0e837c1d6e85289e4f39c92e74e1fb3b96
Joshua-Xavier <j.nelssonsmith@gmail.com>
2016-12-20 09:16:18 +1100

    adding more documentation, still waiting on API update for full testing

--- app/containers/OfferingContainer.jsx
+++ app/containers/OfferingContainer.jsx
@@ -1,11 +1,12 @@
 import React, { Component, PropTypes } from "react";
 
 /**
- * Description
+ * Offering container returns the formatted unit offering/location data
  * 
  * @author JXNS
  * 
- * @param {type} var - blah
+ * @param {array} offeringArray - a multidimensional array of values of the form [[[Location1], [offering1, offering2, ...], [Location2], [offering1, offering2, ...]]] etc
+ * 
  */
 
 const testData = [[['Clayton'], ['First semester 2017 (Day)', 'Second semester 2017 (Day)']], [['Malaysia'], ['First semester 2017 (Day)']], [['South Africa'], ['First semester 2017 (Day)']]];
@@ -13,7 +14,8 @@ const testData = [[['Clayton'], ['First semester 2017 (Day)', 'Second semester 2
 export default class OfferingContainer extends Component {
 
     /**
-     * 
+     * If the offeringArray given has any values (length > 0) then notAvailable is set to false and the offerings can be output, otherwise in the case of an empty array,
+     * the component will just return "no offering information available" as seen in the render method.
      */
     constructor(props) {
         super(props);
@@ -32,7 +34,7 @@ export default class OfferingContainer extends Component {
     }
 
     /**
-     * Render description
+     * If the data is not available it renders a message indicating as much, otherwise it maps through the data and renders it.
      */
     render() {
 
