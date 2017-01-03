845b81054b81904348c834f336b07f9d923b5c1a
lorderikir <ericjiang97@gmail.com>
2017-01-03 14:25:15 +1100

    update to fuzzy search

--- app/utils/fuzzy.js
+++ app/utils/fuzzy.js
@@ -4,21 +4,19 @@ import Fuse from "fuse.js";
 /**
  * Fuzzy Search algorithm
  */
-export default class fuzzy {
-  static main(searchItem){
-      var options = {
-        include: ["score"],
-        shouldSort: true,
-        threshold: 0.6,
-        location: 0,
-        distance: 100,
-        maxPatternLength: 32,
-        minMatchCharLength: 1,
-        keys:["UnitCode", "UnitName"]
-      };
-      var data = UnitQuery.getUnitCodeAndUnitNames();
-      var fuse = Fuse(data, options);
+export function main(searchItem){
+    var options = {
+      include: ["score"],
+      shouldSort: true,
+      threshold: 0.6,
+      location: 0,
+      distance: 100,
+      maxPatternLength: 32,
+      minMatchCharLength: 1,
+      keys:["UnitCode", "UnitName"]
+    };
+    var data = UnitQuery.getUnitCodeAndUnitNames();
+    var fuse = Fuse(data, options);
 
-      return fuse.search(searchItem);
-  }
+    return fuse.search(searchItem);
 }
