15f049a647464a9384c9639894317c45aa0ba15e
Joshua-Xavier <j.nelssonsmith@gmail.com>
2017-01-04 14:11:50 +1100

    linting CourseTemplate

--- app/utils/CourseTemplate.js
+++ app/utils/CourseTemplate.js
@@ -10,20 +10,19 @@ export default class CourseTemplate {
      * 
      */
     static parse(data) {
-        console.log(data);
-
         let newTeachingPeriods = [];
         let newCost = 0;
         let newCP = 0;
         let tmpArr = data.teachingPeriods;
-        console.log(tmpArr)
         let max = 4;
+        
         for (let m=0; m < tmpArr.length; m++) {
             let item = tmpArr[m];
             if (item.numberOfUnits > max){
                 max = item.numberOfUnits;
             }
         }
+
         for (let i=0; i < tmpArr.length; i++) {
             console.log("hello")
             let item = tmpArr[i];
@@ -34,11 +33,8 @@ export default class CourseTemplate {
                     for (let k=0; k < item.numberOfUnits; k++){
                         let unit = item.units[k];
                         unit.Cost = CostCalc.calculateCost(unit.SCABand, unit.CreditPoints);
-                        console.log("---" + unit.UnitCode + "---");
                         newCost += unit.Cost;
                         newCP += unit.CreditPoints;
-                        console.log("Cost: " + newCost);
-                        console.log("CP: " + newCP);
                     }
                     for(let j=0; j < max - item.numberOfUnits; j++){
                         item.units.push(null);
@@ -54,8 +50,7 @@ export default class CourseTemplate {
             newCP: newCP,
             overLoadNumber: max
         }
-        //let scaMap = [0, 132, 188, 220]; //sca band 1 through 3 maps to their per credit point cost
-        //let estCost = scaMap[SCAband] * creditPoints;
+
         return result;
     }
 }
