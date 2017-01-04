1e0356ae631138849eec6b92ffe70bd9f11e44b8
lorderikir <ericjiang97@gmail.com>
2017-01-04 11:06:08 +1100

    updated 404 page

--- app/components/base/404.jsx
+++ app/components/base/404.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import { Image, Segment } from "semantic-ui-react";
 
 const missingPage = () =>
-  <Segment textAlign='center' padded="very" style={{height: "315px", background: 'url(../../resources/img/notthepage.jpg)', backgroundColor:'rgba(52,52,52,0)'}}>
-    <h3 style={{color: 'yellow'}}>This is not the page you are looking for</h3>
+  <Segment textAlign='center' padded="very" style={{height: "900px", background: 'url(../../resources/img/hal.png)', backgroundColor:'rgba(52,52,52,0)'}}>
+    <h3 style={{color: 'white'}}>I'm sorry Dave, I'm afraid I can't let you in.</h3>
+    <p style={{color: 'white'}}>This page is missing or does not exist</p>
+    <a href="./">Click Here to Go to the Home Page</a>
   </Segment>;
 
 
