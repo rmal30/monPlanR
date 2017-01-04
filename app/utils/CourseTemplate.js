7f2297ada4765ba984bf9f6185bf1b0669a23b47
Joshua-Xavier <j.nelssonsmith@gmail.com>
2017-01-04 14:18:09 +1100

    docs for coursetemplate

--- app/utils/CourseTemplate.js
+++ app/utils/CourseTemplate.js
@@ -7,7 +7,9 @@ import CostCalc from "./CostCalc";
 export default class CourseTemplate {
 
     /**
-     * 
+     * parse takes a course template object as returned by the api as 'data' and creates a valid teachingPeriod array.
+     * Because of certain issues with the API's data integrity it is necessary to parse this data and catch any invalid 
+     * pieces of data so the table is not corrupted. '
      */
     static parse(data) {
         let newTeachingPeriods = [];
@@ -16,6 +18,8 @@ export default class CourseTemplate {
         let tmpArr = data.teachingPeriods;
         let max = 4;
         
+        // Currently the easiest way to check for overloading so we render the correct amount of teaching period unit slots
+        // This loops through the teaching periods and if the number of units in a teaching period is greater than 4, makes that the new max
         for (let m=0; m < tmpArr.length; m++) {
             let item = tmpArr[m];
             if (item.numberOfUnits > max){
@@ -23,12 +27,16 @@ export default class CourseTemplate {
             }
         }
 
+        /**
+         * Goes through each teaching period and if the number of units is 0, fills it with nulls.
+         * If a unit is encountered, it loops through every unit and calculates the cost, updating the credit point and 
+         * total cost values as well.
+         */
         for (let i=0; i < tmpArr.length; i++) {
-            console.log("hello")
             let item = tmpArr[i];
             if (item.code){
                 if (item.numberOfUnits === 0) {
-                    item.units = new Array(4).fill(null);    
+                    item.units = new Array(max).fill(null);    
                 } else {
                     for (let k=0; k < item.numberOfUnits; k++){
                         let unit = item.units[k];
@@ -44,6 +52,8 @@ export default class CourseTemplate {
             }
         }
 
+
+        // result object contains all the data we want
         let result = {
             newTeachingPeriods: newTeachingPeriods,
             newCost: newCost,
