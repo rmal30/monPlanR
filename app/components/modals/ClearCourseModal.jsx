8d012aa35befefc9c80d44438a5a5ee3cd2ae57c
Joshua-Xavier <j.nelssonsmith@gmail.com>
2017-01-04 16:01:21 +1100

    Made clear course disabled when an empty template is used

--- app/components/modals/ClearCourseModal.jsx
+++ app/components/modals/ClearCourseModal.jsx
@@ -65,7 +65,7 @@ class ClearCourseModal extends Component {
      */
     render() {
         return (
-            <Modal trigger={<Button className="no-print" fluid={this.props.fluid} color="red" onClick={this.handleOpen.bind(this)}>Clear course</Button>}
+            <Modal trigger={<Button className="no-print" disabled={this.props.disabled} fluid={this.props.fluid} color="red" onClick={this.handleOpen.bind(this)}>Clear course</Button>}
                 open={this.state.modalOpen}
                 onClose={this.handleClose.bind(this)}>
                 <Modal.Header>
