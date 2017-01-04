e594d758930260a545223542ccf8d013203bff15
Joshua-Xavier <j.nelssonsmith@gmail.com>
2017-01-05 09:59:16 +1100

    added fuzzysearch documentation

--- app/utils/FuzzySearch.js
+++ app/utils/FuzzySearch.js
@@ -1,12 +1,18 @@
 import Fuse from "fuse.js";
 
 /**
- * 
+ * @author JXNS
  */
 export default class FuzzySearch {
 
     /**
-     * Retrieves a list of units locally specifying only unit names and codes.
+     * search takes in a number of params and returns an array of results from a fuzzy search backend.
+     * Please note that these results return the score key so the actual results are accessed via results[index].item
+     * @param {string} searchTarget - the string you are searching for against the data
+     * @param {array} data - the array of objects that you are searching against, please note that these objects should all have keys from your searchKeys array
+     * @param {integer} numberOfResults - the number of results you want to return
+     * @param {array} searchKeys - an array of strings that should be attibutes of your data objects, these keys will be searched against in the data set for matches
+     *                             to the searchTarget
      */
     static search(searchTarget, data, numberOfResults, searchKeys) {
         if(searchTarget !== null || searchTarget !== "") {
