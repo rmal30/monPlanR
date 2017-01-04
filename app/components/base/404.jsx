77a35c987f8f20f73ae1ab475fdb0f8d478816df
lorderikir <ericjiang97@gmail.com>
2017-01-04 14:20:52 +1100

    patch for updated 404 page, updated loading messages

--- app/components/base/404.jsx
+++ app/components/base/404.jsx
@@ -1,15 +1,24 @@
 import React from "react";
-import { Segment } from "semantic-ui-react";
+import { Segment, Button, Icon} from "semantic-ui-react";
 import {Link} from "react-router";
 
 const missingPage = () =>
   <Segment textAlign='center' padded="very"
     style={{minHeight: "100%", width: "100%" ,background: 'url(../../resources/img/hal.png)no-repeat', backgroundPosition: 'center', backgroundSize: 'cover', backgroundColor:'rgba(52,52,52,0)'}}>
+    <div>
+      <h3 style={{color: 'white'}}>I'm sorry Dave, I'm afraid I can't let you in.</h3>
+      <p style={{color: 'white'}}>This page is missing or does not exist</p>
+      <p style={{color: 'white'}}>Error Code: 404</p>
+      <Link to="/">
+        <Button color="blue">
+          <Icon name='home' />
+          Click Here to Go to the Home Page
+        </Button>
+      </Link>
+    </div>
 
-    <h3 style={{color: 'white'}}>I'm sorry Dave, I'm afraid I can't let you in.</h3>
-    <p style={{color: 'white'}}>This page is missing or does not exist</p>
-    <Link to="/">Click Here to Go to the Home Page</Link>
-    <div className="push404"></div>
+    <div className="push404">
+    </div>
   </Segment>;
 
 
