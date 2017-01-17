53b1cd2fa091f6d9b17bfccc9f35375caa03f51a
lorderikir <ericjiang97@gmail.com>
2017-01-18 09:11:46 +1100

    security update

--- server.js
+++ server.js
@@ -3,6 +3,39 @@ var path = require('path')
 var compression = require('compression')
 
 var app = express()
+var helmet = require("helmet");
+var hidePoweredBy = require("hide-powered-by");
+var session = require("express-session");
+var nosniff = require("dont-sniff-mimetype");
+var ienoopen = require('ienoopen');
+var xssFilter = require('x-xss-protection');
+var frameguard = require('frameguard');
+var hpkp = require('hpkp');
+var csp = require('helmet-csp');
+
+console.log('Deploying Security Measures')
+app.use(hidePoweredBy({setTo: 'Coffee'}));
+app.use(nosniff());
+app.use(ienoopen());
+app.use(xssFilter());
+app.use(frameguard({action: 'deny'}))
+app.use(hpkp({
+  maxAge: 1209600,
+  sha256s: ['AbCdEf123=', "ZyXwVu456"],
+
+  setIf: function(req,res){
+    return req.secure
+  }
+}));
+app.use(csp({
+  directives: {
+    scriptSrc: ["'self'", "'unsafe-inline'", "www.google-analytics.com"]
+  },
+  reportOnly: false,
+  setAllHeaders: false,
+  disabledAndroid: false,
+  browserSniff: false
+}))
 
 app.use(compression())
 
