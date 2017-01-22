7a80ae30518066b4f36af24fe430cf0f7ef3b443
Joshua-Xavier <j.nelssonsmith@gmail.com>
2017-01-23 08:47:21 +1100

    test breaking

--- app/components/Course/CourseStructure.jsx
+++ app/components/Course/CourseStructure.jsx
@@ -22,7 +22,7 @@ import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import * as counterActions from "../../actions/CounterActions";
 import * as courseActions from "../../actions/CourseActions";
-
+lkjdaghahngalag
 
 /**
  * Set up any props you want course structure to be passed here
