7f0eec579afbf79f86cccae07645eee577c79d28
Joshua-Xavier <j.nelssonsmith@gmail.com>
2017-01-25 09:07:59 +1100

    cleaning up dev dependencies because we can use the redux dev tools now

--- app/store.js
+++ app/store.js
@@ -2,30 +2,13 @@ import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { syncHistoryWithStore } from "react-router-redux";
 import { browserHistory } from "react-router";
-import logger from "redux-logger";
 import promise from "redux-promise-middleware";
 import thunk from "redux-thunk";
 import appReducer from "./reducers/Index";
 
 
-/**
- * Promise middle ware is used a little differently
- * 
- * The initial action should dispatch an action with a payload of a promise
- * 
- * like:
- *      store.dispatch({
- *          type: "FOO"
- *          payload: axios.get("""")
- * })
- * 
- * This then broken up into 
- * FOO_PENDING
- * FOO_FULFILLED
- * FOO_REJECTED
- */
 const store = createStore(appReducer, composeWithDevTools(
-    applyMiddleware(promise(), logger(), thunk)));
+    applyMiddleware(promise(), thunk)));
 
 // Named export the history to use 
 export const history = syncHistoryWithStore(browserHistory, store);
