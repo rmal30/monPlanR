2212f7626fc235897f8ee85e3dafaca50b0f7be4
Joshua-Xavier <j.nelssonsmith@gmail.com>
2017-01-27 14:15:48 +1100

    adding a course info placeholder

---
+++ app/components/Course/CourseInfoPlaceholder.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+/**
+ * Currently a placeholder for a placeholder component...
+ * (Someone call Exhibit)
+ */
+const CourseInfoPlaceholder = () => {
+    return (
+        <p>Loading...</p>
+    );
+};
+
+CourseInfoPlaceholder.propTypes = {
+
+};
+
+export default CourseInfoPlaceholder;
