1224d9ef425aa11ed6216b7e6def7b94c72d1633
lorderikir <ericjiang97@gmail.com>
2017-02-02 13:51:33 +1100

    moved more CSS files across from inline styling

--- app/components/base/404.jsx
+++ app/components/base/404.jsx
@@ -10,13 +10,14 @@ import { Link } from "react-router";
  */
 const missingPage = () => (
     <Segment textAlign="center" padded="very"
-        style={{minHeight: "100%", width: "100%" ,background: "url(../../img/hal.png)no-repeat", backgroundPosition: "center", backgroundSize: "cover", backgroundColor:"rgba(52,52,52,0)"}}>
+        className="missingPage"
+        style={{background: "url(../../img/hal.png)no-repeat", backgroundColor: "rgba(52,52,52,0)"}}>
         <div>
-            <h3 style={{color: "white"}}>I'm sorry Dave, I'm afraid I can't let you do that.</h3>
-            <p style={{color: "white"}}>This page is missing or does not exist</p>
-            <p style={{color: "white"}}>Error Code: 404</p>
+            <h3>I'm sorry Dave, I'm afraid I can't let you do that.</h3>
+            <p>This page is missing or does not exist</p>
+            <p><i>Error Code: 404</i></p>
             <Link to="/">
-                <Button color="blue">
+                <Button className="btnmainblue">
                     <Icon name="home" />
                     Click Here to Go to the Home Page
                 </Button>
