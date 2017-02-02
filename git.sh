c2141e15271b8a411e9166504c6fbf3b93232717
lorderikir <ericjiang97@gmail.com>
2017-02-02 11:32:43 +1100

    New Unit Tests

--- git.sh
+++ git.sh
@@ -2,10 +2,10 @@
 
 echo "Preparing for git"
 echo "Cleaning up doc folder..."
-rm -rf doc/
+rm -rf docs/
 rm -rf mochawesome-reports/
 echo "Builing Unit Test Report"
-mocha --compilers babel-core/register test/**/*.spec.js* --reporter mochawesome
+mocha --compilers babel-core/register test/setup.js test/**/*.spec.{js,jsx} --reporter mochawesome
 echo "Renaming Files"
 mv mochawesome-reports docs
 cd docs
