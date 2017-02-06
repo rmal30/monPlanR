6be55bcd80c43c93ecde0fdfb56cb8a64ff73e00
Josh Nelsson-Smith <j.nelssonsmith@gmail.com>
2017-02-07 09:14:48 +1100

    refactored a add unit test to not waste

---
+++ test/actionCreators/CounterActions.spec.js
@@ -0,0 +1,29 @@
+/*
+Not ready to be implemented yet, but some good testing resources shown here:
+https://github.com/reactjs/redux/blob/master/docs/recipes/WritingTests.md
+
+import Counter from "../../app/reducers/Counter";
+import expect from "expect";
+import deepFreeze from "deep-freeze";
+import { describe, it } from "mocha";
+import { incrementCreditPoints } from "../../app/actions/CounterActions";
+
+describe("ACTION-CREATOR: CounterActions", () => {
+    describe("AC: incrementCreditPoints", () => {
+
+    });
+
+    describe("AC: decrementCreditPoints", () => {
+        
+    });
+
+    describe("AC: incrementCost", () => {
+        
+    });
+
+    describe("AC: decrementCosts", () => {
+        
+    });
+});
+
+*/
