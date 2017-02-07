1a8702e72fcdbe6fbfd7b0f028932acd4148d756
Saurabh Joshi <sauj123@hotmail.com>
2017-02-08 09:34:19 +1100

    [dev] added coveralls

---
+++ test/components/Course/CourseStatisticGroup.spec.jsx
@@ -0,0 +1,18 @@
+/*import React from "react";
+import { shallow } from "enzyme";
+
+import CourseStatisticGroup from "../../../app/components/Course/CourseStatisticGroup.jsx";
+
+describe("COMPONENT: CourseStatisticGroup", () => {
+    describe("#creditPoints", () => {
+        const wrapper = shallow(
+            <CourseStatisticGroup
+                creditPoints={72}
+                cost={15600}
+                />
+        );
+
+        expect(wrapper.find(".statistic .value").text().trim()).to.equal("72");
+    });
+});
+*/
