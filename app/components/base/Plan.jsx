b828d8ff704fea17c99e7ffd9ded6994f378d256
Saurabh Joshi <sauj123@hotmail.com>
2017-02-10 10:24:14 +1100

    [dev] change Plan to be stateless component

--- app/components/base/Plan.jsx
+++ app/components/base/Plan.jsx
@@ -1,11 +1,7 @@
-import React, { Component, PropTypes } from "react";
-import { connect } from "react-redux";
+import React, { PropTypes } from "react";
 import { Container } from "semantic-ui-react";
 
 import CustomUnitModal from "../modals/CustomUnitModal.jsx";
-import UnitQuery from "../../utils/UnitQuery";
-import CostCalc from "../../utils/CostCalc";
-//import CourseStructure from "../Course/CourseStructure.jsx";
 import CourseStructure from "../Course/CourseStructure.jsx";
 import NotificationContainer from "../../containers/NotificationContainer.jsx";
 import CourseOverviewContainer from "../../containers/Course/CourseOverviewContainer.jsx";
@@ -14,142 +10,22 @@ import CourseOverviewContainer from "../../containers/Course/CourseOverviewConta
  * The plan component is the main page of the app, where students can add and
  * remove teaching periods and units.
  */
-class Plan extends Component {
-    /**
-     * State holds a unit object that is used during the add
-     * unit process.
-     *
-     * @param {object} props - React's props
-     */
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            unitToAdd: undefined,
-            showAddToCourseUI: false,
-            totalCredits: 0,
-            totalCost: 0,
-            focusedUnitCode: "",
-            customUnitCode: undefined,
-            searchResults: null,
-            searchResultIndex: 0,
-            courseToLoad: "",
-            courseYear: 0
-        };
-
-        this.addToCourse = this.addToCourse.bind(this);
-        this.doneAddingToCourse = this.doneAddingToCourse.bind(this);
-        this.cancelAddingToCourse = this.cancelAddingToCourse.bind(this);
-    }
-
-    /**
-     * Adds unit to course by specifying unit code. It does not add unit
-     * immediately.
-     *
-     * @param {string} unitToAdd - The unit to be added.
-     * @param {boolean} custom - If it is a custom unit, prompt user to enter details
-     */
-    addToCourse(nUnitCode, custom, drag, position) {
-        if(typeof nUnitCode === "string") {
-            if(!custom) {
-                UnitQuery.getExtendedUnitData(nUnitCode)
-                    .then(response => {
-                        let data = response.data;
-                        data.Cost = CostCalc.calculateCost(data.SCABand, data.CreditPoints);
-
-                        this.setState({
-                            unitToAdd: data,
-                            focusedUnitCode: data.UnitCode
-                        });
-
-                    })
-                    .catch(error => {
-                        console.error(error);
-                    });
-
-            } else if(custom && drag) {
-                this.setState({
-                    unitToAdd: {
-                        UnitCode: nUnitCode,
-                        UnitName: "Create custom unit",
-                        custom: true,
-                        dragged: true
-                    },
-                    focusedUnitCode: nUnitCode
-                });
-            } else if(custom) {
-                this.setState({
-                    customUnitCode: nUnitCode,
-                    customUnitPosition: position
-                });
-            }
-        } else if(custom) {
-            this.setState({
-                customUnitCode: "Unit code"
-            });
-        }
-    }
-
-    /**
-     * Shows add unit UI and allows user to add their custom unit.
-     *
-     * @param {string} unitToAdd - The unit to be added.
-     */
-    addCustomUnitToCourse(unitToAdd) {
-        this.setState({
-            unitToAdd
-        });
-    }
-
-    /**
-     * handles the cancellation of addCustomUnitToCourse
-     */
-    cancelAddingCustomUnitToCourse() {
-        this.setState({
-            customUnitCode: undefined
-        });
-    }
-
-
-    /**
-     * Handles the cancellation of adding a unit to course
-     */
-    cancelAddingToCourse() {
-        this.setState({
-            unitToAdd: undefined
-        });
-    }
-
-    /**
-     * Turns off add unit UI, also updated running course totals
-     */
-    doneAddingToCourse() {
-        this.setState({
-            unitToAdd: undefined
-        });
-    }
-
-
+function Plan() {
     /**
      * Returns a container of grid of a single row, holding the course
      * structure.
      */
-    render() {
-        return (
-            <div>
-                <NotificationContainer />
-                <CustomUnitModal
-                    UnitCode={this.state.customUnitCode}
-                    position={this.state.customUnitPosition}
-                    cancelAddingCustomUnitToCourse={this.cancelAddingCustomUnitToCourse.bind(this)}
-                    addCustomUnitToCourse={this.addCustomUnitToCourse.bind(this)} />
-                <Container className="main text">
-                    <CourseOverviewContainer />
-                    <CourseStructure />
-                </Container>
-            </div>
-        );
-    }
+
+    return (
+        <div>
+            <NotificationContainer />
+            <CustomUnitModal />
+            <Container className="main text">
+                <CourseOverviewContainer />
+                <CourseStructure />
+            </Container>
+        </div>
+    );
 }
 
 Plan.propTypes = {
@@ -162,18 +38,7 @@ Plan.propTypes = {
             /* What course the student has selected*/
             courseToLoad: PropTypes.string
         }).isRequired
-    }).isRequired,
-    showingCustomUnitModal: PropTypes.bool
+    }).isRequired
 };
 
-/**
- * Injects showingCustomUnitUI into props, used for displaying custom unit
- * whenever the user wants to add details in for their custom unit.
- */
-const mapStateToProps = state => {
-    return  {
-        showingCustomUnitModal: state.UI.showingCustomUnitModal
-    };
-};
-
-export default connect(mapStateToProps)(Plan);
+export default Plan;
