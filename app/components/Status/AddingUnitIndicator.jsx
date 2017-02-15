4b79684b1ea1019e130166e7a27c7396a9fe722c
Josh Nelsson-Smith <j.nelssonsmith@gmail.com>
2017-02-15 16:02:43 +1100

    updating the indicators for consistency

--- app/components/Status/AddingUnitIndicator.jsx
+++ app/components/Status/AddingUnitIndicator.jsx
@@ -1,5 +1,6 @@
 import React, { PropTypes } from "react";
 import { Grid, Icon, Segment, Button } from "semantic-ui-react";
+import CourseStatisticGroupContainer from "../../containers/Course/CourseStatisticGroupContainer";
 
 /**
  * A simple component for letting the user know that they are adding a unit
@@ -9,10 +10,13 @@ const AddingUnitIndicator = ({ unitCode, onCancelAddingUnit }) => {
     return (
         <Segment color="green" className="adding">
             <Grid stackable>
-                <Grid.Column width={12}>
+                <Grid.Column width={10}>
                     <h3><Icon name="add" />Adding {unitCode || "unit"}</h3>
                     <h5>Place the unit somewhere in your course below</h5>
                     <Button className="btnlightcancel" onClick={onCancelAddingUnit}>Cancel Adding Unit</Button>
+                </Grid.Column>
+                <Grid.Column width={6}>
+                    <CourseStatisticGroupContainer />
                 </Grid.Column>
             </Grid>
         </Segment>
--- app/components/Status/MovingUnitIndicator.jsx
+++ app/components/Status/MovingUnitIndicator.jsx
@@ -1,5 +1,6 @@
 import React, { PropTypes } from "react";
 import { Grid, Icon, Segment, Button } from "semantic-ui-react";
+import CourseStatisticGroupContainer from "../../containers/Course/CourseStatisticGroupContainer";
 
 /**
  * A simple component for letting the user know that they are moving a unit
@@ -9,10 +10,13 @@ const MovingUnitIndicator = ({ unitCode }) => {
     return (
         <Segment color="blue" className="moving">
             <Grid stackable>
-                <Grid.Column width={12}>
+                <Grid.Column width={10}>
                     <h3><Icon name="move" />Moving {unitCode || "unit"}</h3>
                     <h5>Drop the unit in an empty cell, or on another unit to swap the units</h5>
                     <Button className="btnlightcancel">Cancel Moving Unit</Button>
+                </Grid.Column>
+                <Grid.Column width={6}>
+                    <CourseStatisticGroupContainer />
                 </Grid.Column>
             </Grid>
         </Segment>
