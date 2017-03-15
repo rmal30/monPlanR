5c9be1ac583adf39bf891e3eb047f4a3776e276e
Josh Nelsson-Smith <j.nelssonsmith@gmail.com>
2017-03-16 09:01:54 +1100

    docs for unitcache

--- app/reducers/UnitCacheReducer.js
+++ app/reducers/UnitCacheReducer.js
@@ -7,6 +7,10 @@ const defaultState = {
 
 /**
  * @author JXNS
+ * The unit cache is just as it sounds, a cache for units. The api calls for unit info are not hugely 
+ * costly, however users often make many unit info calls so it is in our interest to cache these results.
+ * The cache currently operates as a simple initiliase on refresh cache, but I have designed it to be easily 
+ * portable to a local storage saved cache in future (checking for stale data via the time of access values)
  */
 const UnitCacheReducer = (state = defaultState, action) => {
     switch(action.type) {
