41a985a604e9cd249ca1b56164f95efa619e1d77
lorderikir <ericjiang97@gmail.com>
2017-03-23 16:13:59 +1100

    Updated Site

--- app/config/routes.js
+++ app/config/routes.js
@@ -8,6 +8,8 @@ import Home from "../components/Pages/Home.jsx";
 import Plan from "../components/Pages/Plan.jsx";
 import Graph from "../components/Pages/Graph.jsx";
 import Future from "../components/Pages/myFuture/Future.jsx";
+import Commerce from "../components/Pages/myFuture/Commerce.jsx";
+import CareerGD from "../components/Pages/myFuture/Career-GameDev.jsx";
 import View from "../components/Pages/View.jsx";
 import YearFormContainer from "../containers/Forms/YearFormContainer.jsx";
 import MissingPage from "../components/Pages/404.jsx";
@@ -17,6 +19,8 @@ export default (
     <Route path="/" component={Main}>
         <IndexRoute darkBackground component={Home} />
         <Route path="future" myFuture noFooter noNav component={Future} />
+        <Route path="future/commerce" myFuture noFooter noNav component={Commerce} />
+        <Route path="future/career/GameDev" myFuture noFooter noNav component={CareerGD} />
         <Route path="yearForm" darkBackground component={YearFormContainer} />
         <Route path="plan(/?courseDetails)" noFooter component={Plan} handler={Home} showAddUnit showStatus />
         <Route path="graph" noFooter component={Graph} handler={Home} showAddUnit />
