23e71ccca3b5cb5d6e9caf244c27c9ef46ba8211
lorderikir <ericjiang97@gmail.com>
2017-04-05 13:01:41 +1000

    updated files, removed economist

--- app/config/routes.js
+++ app/config/routes.js
@@ -10,7 +10,6 @@ import Graph from "../components/Pages/Graph.jsx";
 import Future from "../components/Pages/myFuture/Future.jsx";
 import CareerSelect from "../components/Pages/myFuture/CareerSelect.jsx";
 import Commerce from "../components/Pages/myFuture/Commerce.jsx";
-import CEconomist from "../components/Pages/myFuture/Career-Economist.jsx";
 import View from "../components/Pages/View.jsx";
 import YearFormContainer from "../containers/Forms/YearFormContainer.jsx";
 import MissingPage from "../components/Pages/404.jsx";
@@ -23,7 +22,6 @@ export default (
         <Route path="future" myFuture noFooter noNav component={Future} />
         <Route path="future/select" myFuture noFooter noNav component={CareerSelect} />
         <Route path="future/commerce" myFuture noFooter noNav component={Commerce} />
-        <Route path="future/career/economist" myFuture noFooter noNav component={CEconomist} />
         <Route path="future/career/:careerID" noFooter myFuture noNav handler={CareerDetailContainer} component={CareerDetailContainer} />
         <Route path="yearForm" darkBackground component={YearFormContainer} />
         <Route path="plan(/?courseDetails)" noFooter component={Plan} handler={Home} showAddUnit showStatus />
